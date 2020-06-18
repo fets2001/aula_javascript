777749cbc2b8a864eafb84e9f82e534b3517f758
André Dias <aluiz207@outlook.com>
2020-06-18 16:41:15 -0300

    versão 1

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var idade = prompt("Digite sua idade");
+
+if (idade >= 18) {
+
+    alert("Você é maior de idade pois tem " + idade);
+
+} else{
+
+    alert("Você é menor de idade pois tem " + idade);
+}
+
+
+
